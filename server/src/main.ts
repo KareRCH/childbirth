56a0412a013023cca2b8548999506ed8ea20e6e6
Lee Hak Jun <harleybird23@gmail.com>
2022-06-28 07:02:03 +0900

    interval

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import axios from 'axios';
+
+const sec = 1000;
+const min = 60 * sec;
+var url;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
+  url = await app.getUrl();
 }
 bootstrap();
+
+setInterval(async () => {
+  await axios.get(url + "/api")
+  .then().catch();
+  
+}, 25*min);
