dbe7b17ca3b6b87d1624209ede5e766a2da4a2f8
Lee Hak Jun <harleybird23@gmail.com>
2022-06-28 16:51:09 +0900

    major

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '../img/logo-banner.png';
+
 const Header = () => {
     return (
         <nav className="navbar">
             <div className="navbar_logo">
-                <i className="fa-solid fa-handshake-simple"></i>
-                <Link to='/'>임산부 지원 안내 서비스</Link>
+                <Link to='/'>
+                    <img src={logo} />
+                </Link>
             </div>
-            <ul className="navbar_menu">
-                <li><Link to="/goverment">중앙정책</Link></li>
-                <li><Link to="/maplinker">지역구정책</Link></li>
-                <li><Link to="/plussites">관련사이트</Link></li>
-                <li><a href="https://kbucall.org/sub.do?menu_no=5&c_no=21" target="_blank" rel="noreferrer">문의</a></li>
-            </ul>
+            <div className="menu_wrap">
+                <div className="menu_inner_wrap">
+                    <ul className="navbar_menu">
+                        <li><Link to="/goverment">중앙정책</Link></li>
+                        <li><Link to="/maplinker">지역구정책</Link></li>
+                        <li><Link to="/plussites">관련사이트</Link></li>
+                        <li><a href="https://linc.hannam.ac.kr/main.do" target="_blank" rel="noreferrer">문의</a></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 }
